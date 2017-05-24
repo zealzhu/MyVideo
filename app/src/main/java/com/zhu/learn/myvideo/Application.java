5276ca6575ee1ff7ab7eeadee06713a9ad0df627
zhu <z5130761@gmail.com>
2017-05-24 22:06:31 +0800

    不使用libbili，将APPKEY和SECRET直接写成明文

--- app/src/main/java/com/zhu/learn/myvideo/Application.java
+++ app/src/main/java/com/zhu/learn/myvideo/Application.java
@@ -1,9 +1,6 @@
 package com.zhu.learn.myvideo;
 
-import android.content.Context;
-
 import io.vov.vitamio.utils.Log;
-import tv.danmaku.bili.api.BilibiliUtil;
 
 /**
  * Created by zhu on 2017/5/23.
@@ -11,14 +8,12 @@ import tv.danmaku.bili.api.BilibiliUtil;
 
 public class Application extends android.app.Application {
     private static final String TAG = "Application";
-    public static String APP_KEY;
-    public static String SECRET;
+    public static String APP_KEY = "1d8b6e7d45233436";
+    public static String SECRET = "560c52ccd288fed045859ed18bffd973";
     @Override
     public void onCreate() {
         super.onCreate();
         Log.d(TAG, "onCreate");
-        APP_KEY = BilibiliUtil.getAppKey();
-        SECRET = BilibiliUtil.getSecret();
     }
 
 }
--- app/src/main/java/com/zhu/learn/myvideo/MainActivity.java
+++ app/src/main/java/com/zhu/learn/myvideo/MainActivity.java
@@ -17,7 +17,6 @@ import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
 import com.zhu.learn.myvideo.adapt.PagerViewFragmentAdapt;
